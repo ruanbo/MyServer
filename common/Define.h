2c1d2e010431e4e50759e86787a31eb46fc399bf
RuanBo <ruanbosunny@163.com>
2014-04-09 20:21:14 +0800

    common folder

---
+++ common/Define.h
@@ -0,0 +1,14 @@
+/*
+ * Define.h
+ *
+ *  Created on: Apr 9, 2014
+ *      Author: Robert
+ */
+
+#ifndef DEFINE_H_
+#define DEFINE_H_
+
+
+
+
+#endif /* DEFINE_H_ */
---
+++ server/main.cpp
@@ -0,0 +1,10 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Apr 9, 2014
+ *      Author: Robert
+ */
+
+
+
+
