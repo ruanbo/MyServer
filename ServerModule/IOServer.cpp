01dc9c3a7aababa499059a7572af760a8fc9f413
RuanBo <ruanbosunny@163.com>
2014-04-12 11:33:09 +0800

    to do: io server

---
+++ ServerModule/IOServer.cpp
@@ -0,0 +1,18 @@
+/*
+ * IOServer.cpp
+ *
+ *  Created on: Apr 12, 2014
+ *      Author: Robert
+ */
+
+#include "IOServer.h"
+
+IOServer::IOServer() {
+	// TODO Auto-generated constructor stub
+
+}
+
+IOServer::~IOServer() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ ServerModule/IOServer.h
@@ -0,0 +1,23 @@
+/*
+ * IOServer.h
+ *
+ *  Created on: Apr 12, 2014
+ *      Author: Robert
+ */
+
+#ifndef IOSERVER_H_
+#define IOSERVER_H_
+
+
+
+class IOServer {
+public:
+	IOServer();
+	virtual ~IOServer();
+};
+
+
+
+
+
+#endif /* IOSERVER_H_ */
